b002c9cb086b3735c720f35a773154cfd96f1173
Kuzanity <adriangabrieldiaz07@gmail.com>
2021-11-17 14:59:01 +0000

    With DB

--- Software/web.py
+++ Software/web.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, request
 import urllib.parse
 import requests
+import sqlalchemy as db
 
 app = Flask(__name__)
+engine = db.create_engine('sqlite:///test.sqlite') #Create test.sqlite automatically
+connection = engine.connect()
+metadata = db.MetaData()
+
+Student = db.Table('StudentData', metadata,
+              db.Column('Id', db.Integer()),
+              db.Column('name', db.String(255), nullable=False),
+              db.Column('yearlvl', db.String(5),nullable=False),
+              db.Column('password', db.String(255), default=True)
+              )
+
+metadata.create_all(engine) #Creates the table
 
 @app.route('/')
 def index():
@@ -32,4 +45,4 @@ def test():
     return render_template('test.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5050)
+    app.run(host="0.0.0.0", port=2020)
