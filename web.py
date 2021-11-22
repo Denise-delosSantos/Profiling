d3c3cbbe915fa7160e665f6a1b9764bdb88d1ee1
AndreEmbuido <90046988+AndreEmbuido@users.noreply.github.com>
2021-11-22 14:39:56 +0000

    Add files via upload

--- web.py
+++ web.py
@@ -12,9 +12,24 @@ def index():
 @app.route('/log', methods=['GET', 'POST'])
 def log():
     if request.method == 'POST':
-        username =request.form['user']
-        password =request.form['password']
-    return render_template('login.html')
+        try:
+            username =request.form['user']
+            password =request.form['password']
+            
+            with sql.connect("profile.db") as con:
+                cur = con.cursor()
+                statement = f"SELECT user from students WHERE user='{email_add}' AND password='{password}';"
+                cur.execute(statement)
+                if not cur.fetchone():
+                    print("Login Failed")
+                else:
+                    print("Welcome")
+        except:
+            con.rollback()
+        
+        finally:
+            return render_template("login.html")
+            con.close()
 
 @app.route('/reg')
 def reg():
@@ -23,23 +38,22 @@ def reg():
 @app.route('/addrec',methods = ['POST','GET'])
 def addrec():
     if request.method == 'POST':
+        try:
+            student_id = request.form['student_id']
+            email = request.form['email']
+            password = request.form['password']
 
-        student_id = request.form['student_id']
-        email = request.form['email']
-        password = request.form['password']
+            with sql.connect("profile.db") as con:
+                cur = con.cursor()
 
-        with sql.connect("database.db") as con:
-            cur = con.cursor()
-
-            cur.execute("""INSERT INTO students (student_id, email_add, password) VALUES (?,?,?)""",(student_id,email,password))
-            con.commit()
-            msg = "Success"
+                cur.execute("""INSERT INTO students (student_id, email_add, password) VALUES (?,?,?)""",(student_id,email,password))
+                con.commit()
+        except:
+            con.rollback()
         
-        con.rollback()
-        msg = "Error"
-        
-        return render_template("login.html",msg=msg)
-        con.close()
+        finally:
+            return render_template("login.html")
+            con.close()
 
 @app.route('/page')
 def page():
